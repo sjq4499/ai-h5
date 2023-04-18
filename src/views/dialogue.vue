e141565ddecae558f7707db13eb764981e410b7e
sjq4499 <sjq4499@163.com>
2023-04-18 14:18:05 +0800

    历史

--- src/views/dialogue.vue
+++ src/views/dialogue.vue
@@ -17,7 +17,9 @@
     </div>
     <div class="send-input">
       <div class="send-box">
+        <van-icon class="history" name="wap-nav" @click="show" />
         <van-field
+          class="input"
           v-model="value"
           label=""
           left-icon="smile-o"
@@ -28,6 +30,21 @@
         </div>
       </div>
     </div>
+    <van-popup
+      v-model:show="showLeft"
+      position="left"
+      :style="{ width: '80%', height: '100%' }"
+    >
+      <div class="m-popup">
+        <div
+          class="popup-list"
+          v-for="(item, index) in messageList"
+          :key="index"
+        >
+          {{ item.messageContent }}
+        </div>
+      </div>
+    </van-popup>
     <!-- <Tabbar></Tabbar> -->
   </div>
 </template>
@@ -55,21 +72,27 @@ export default defineComponent({
       {
         messageContent: `Prompt逆向工程是指从某个人工智能（AI）模型的响应中推断出它可能使用的Prompt的过程。这个过程可以帮助我们更好地理解模型是如何工作的，并且可以帮助我们提高模型的性能。下面是进行Prompt逆向工程的一些步骤：
 
-收集数据：首先，需要收集模型生成的一些示例数据。这些数据应该涵盖不同的主题，问题和回答类型。
+        收集数据：首先，需要收集模型生成的一些示例数据。这些数据应该涵盖不同的主题，问题和回答类型。
 
-生成Prompt候选：通过检查模型生成的示例数据，尝试构建可能被模型使用的Prompt的候选列表。
+        生成Prompt候选：通过检查模型生成的示例数据，尝试构建可能被模型使用的Prompt的候选列表。
 
-进行筛选：对候选Prompt进行筛选，并选择那些最有可能被模型使用的Prompt。
+        进行筛选：对候选Prompt进行筛选，并选择那些最有可能被模型使用的Prompt。
 
-进行测试：使用筛选后的Prompt进行测试，观察模型的响应并检查其质量。如果模型的响应质量不佳，则可以进一步调整和优化Prompt。
+        进行测试：使用筛选后的Prompt进行测试，观察模型的响应并检查其质量。如果模型的响应质量不佳，则可以进一步调整和优化Prompt。
 
-重复以上步骤：多次尝试上述步骤，直到找到一个`,
+        重复以上步骤：多次尝试上述步骤，直到找到一个`,
       },
     ]);
     let value = ref('');
+    let showLeft = ref(false);
+    let show = () => {
+      showLeft.value = true;
+    };
     return {
       messageList,
       value,
+      showLeft,
+      show,
     };
   },
 });
@@ -85,6 +108,20 @@ export default defineComponent({
   right: 0;
   .send-box {
     position: relative;
+    display: flex;
+    background: #fff;
+    align-items: center;
+    .history {
+      padding: 0 12px;
+      font-size: 20px;
+      border-right: 1px #000 solid;
+    }
+    .history {
+      margin-right: 4px;
+    }
+    .input {
+      flex: 1;
+    }
   }
   .send-btn {
     position: absolute;
@@ -155,6 +192,17 @@ export default defineComponent({
     margin-left: 48px !important;
     margin-right: 0 !important;
     text-align: start;
+  }
+}
+.m-popup {
+  padding: 24px;
+  .popup-list {
+    border-radius: 4px;
+    background: #515151;
+    padding: 10px 12px;
+    color: #fff;
+    display: inline-block;
+    margin-bottom: 12px;
   }
 }
 </style>
